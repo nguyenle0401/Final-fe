7171e87e9a2b47acc09710fccc4dec5e772e013d
Nguyen Le <nle20959@gmail.com>
2020-09-30 12:57:08 +0700

    set time

--- src/redux/actions/alert.actions.js
+++ src/redux/actions/alert.actions.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 import * as types from "../constants/alert.constants";
 
-const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
+const setAlert = (msg, alertType, timeout = 3000) => (dispatch) => {
   const id = uuidv4();
   dispatch({
     type: types.SET_ALERT,
